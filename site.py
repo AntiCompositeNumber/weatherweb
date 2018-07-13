5376ae3a39d44d47c34a2e23111e97a0a8017303
rj-laptop <rj@rj-laptop.local>
2018-07-13 10:40:18 -0400

    Add template support

--- site.py
+++ site.py
@@ -2,13 +2,17 @@
 
 import web
 
+render = web.template.render('templates/')
+
 urls = (
     '/', 'index'
 )
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        temp = 72
+        humidity = 23
+        return render.index(temp,humidity)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
