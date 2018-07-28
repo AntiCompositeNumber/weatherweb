7d43c1cd411d2e2e27a571f16fb40a4a0736a19a
rj-pi3 <alarm@rj-pi3.local>
2018-07-28 02:10:47 +0000

    Things are working now, data collection possible and stable. Loops.

--- data.py
+++ data.py
@@ -2,21 +2,23 @@
 
 import rrdtool
 import Adafruit_DHT
+import time
 
 #Set up sensor
 sensor = Adafruit_DHT.AM2302
 pin = 17
 
-#Get data from sensor
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+while True:
+    #Get data from sensor
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-#Convert temperature from Celsius to Fahrenheit
-temperature = (temperature * 9/5) + 32
+    #Convert temperature from Celsius to Fahrenheit
+    temperature = (temperature * 9/5) + 32
 
-#Round to tenths
-#temperature = round(temperature,1)
-#humidity = round(humidity,1)
+    #The RRD stores everything as floating-point numbers, so don't bother rounding anything here. 
+    #Send data to RRD
+    rrdtool.update('weatherweb.rrd','N:{0}:{1}'.format(temperature, humidity))
+    print(time.time(),'N:{0}:{1}'.format(temperature,humidity))
 
-#Send data to RRD
-rrdtool.update('weatherweb.rrd','N:{0}:{1}'.format(temperature, humidity))
-print('N:{0}:{1}'.format(temperature,humidity))
+    #And now we wait.
+    time.sleep(55)
