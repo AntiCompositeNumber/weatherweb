5537ba1a925435cd1152750b2a0f7c54cd9caa63
AntiCompositeNumber <anticompositenumber@gmail.com>
2019-02-16 14:53:05 -0500

    Add connection lost indicatior and favicon

--- site.py
+++ site.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python2
 
-import web
-import rrdtool
 import json
 from datetime import datetime
-import subprocess 
+import subprocess
+import web
+import rrdtool
 
-rrdPath='/home/alarm/git/weatherweb/weatherweb.rrd'
+rrdPath = 'weatherweb.rrd'
 render = web.template.render('templates/')
 
 urls = (
-  '/', 'index',
-  '/api/current', 'currentAPI',
-  '/api/day', 'dayAPI'
-  )
+    '/', 'index',
+    '/api/current', 'currentAPI',
+    '/api/day', 'dayAPI'
+    )
 
 class index:
-  def GET(self):
-    return render.index()
+    def GET(self):
+        return render.index()
 
 class currentAPI:
-  def GET(self):
-    lastUpdate = rrdtool.lastupdate(rrdPath)
-    current = {"date":datetime.isoformat(lastUpdate["date"]) + "Z", "temp":round(lastUpdate["ds"]["temp"], 1), "humidity":round(lastUpdate["ds"]["humidity"], 0)}
-    currentJSON = json.dumps(current) 
-    return currentJSON
+    def GET(self):
+        lastUpdate = rrdtool.lastupdate(rrdPath)
+        current = {"date":datetime.isoformat(lastUpdate["date"]) + "Z", "temp":round(lastUpdate["ds"]["temp"], 1), "humidity":round(lastUpdate["ds"]["humidity"], 0)}
+        currentJSON = json.dumps(current)
+        return currentJSON
 class dayAPI:
-  def GET(self):
-    lastDay = subprocess.check_output("/home/alarm/git/weatherweb/day.sh") 
-    return lastDay
+    def GET(self):
+        lastDay = subprocess.check_output("/home/alarm/git/weatherweb/day.sh")
+        return lastDay
 
 if __name__ == "__main__":
-  app = web.application(urls, globals())
-  app.run()
-
-
+    app = web.application(urls, globals())
+    app.run()
